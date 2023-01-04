78129fde25d9b51be70d0e4b114cb08eee15d1db
Adv-Luke-Webster <107184793+Adv-Luke-Webster@users.noreply.github.com>
2023-01-04 17:45:01 +0000

    Added dbMime service - refactored

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const router = express.Router();
 const cors = require("cors");
 
@@ -16,6 +17,7 @@ router.use(
 const dbConnectService = require("../bin/services/dbConnector");
 const dbQueriesService = require("../bin/services/dbQueries");
 const adsConnectService = require("../bin/services/adsConnect");
+const dbMimeReaderService = require("../bin/services/dbMime");
 
 router.get("/dbConnect", dbConnectService.dbConnect);
 router.get("/dbDisConnect", dbConnectService.dbDisConnect);
@@ -23,5 +25,6 @@ router.get("/dbDisConnect", dbConnectService.dbDisConnect);
 router.get("/getTables", dbQueriesService.getTables);
 
 router.get("/adsConnect", adsConnectService.adsConnect);
+router.post("/dbMimeReader", dbMimeReaderService.dbMime);
 
 module.exports = router;
